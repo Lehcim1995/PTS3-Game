bf00e6fc53b63018b06e710f1a50e61c97bdaa48
Lehcim1995 <micheljacobs@hotmail.nl>
2016-09-27 14:22:10 +0200

    Start Game

---
+++ core/src/Interfaces/GameObject.java
@@ -0,0 +1,27 @@
+package Interfaces;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Shape;
+
+/**
+ * Created by michel on 27-9-2016.
+ */
+public interface GameObject
+{
+    Texture GetTexture();
+    void SetTexture(Texture tex);
+
+    Vector2 GetPosition();
+    void SetPosition(Vector2 pos);
+
+    float GetRotation();
+    void SetRotation(float rot);
+
+    Shape GetBoundingShape();
+    void SetBoundingShape(Shape shape);
+
+    void OnCollisionEnter();
+    void OnCollisionExit();
+    void OnCollisionStay();
+}
--- core/src/com/asom/mygame/MainClass.java
+++ core/src/com/asom/mygame/MainClass.java
@@ -2,32 +2,86 @@ package com.asom.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
-public class MainClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class MainClass extends ApplicationAdapter
+{
+    SpriteBatch batch;
+    Texture img;
+    Vector2 position;
+    Vector2 mouse;
+    private Camera camera;
+    private ShapeRenderer shapeRenderer;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create()
+    {
+        batch = new SpriteBatch();
+        img = new Texture(Gdx.files.internal("D:\\Fontys ICT\\Jaar 2\\Java\\PTS3-Game\\core\\assets\\badlogic.jpg"));
+        position = new Vector2(0, 0);
+        mouse = new Vector2(0, 0);
+        camera = new OrthographicCamera();
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    @Override
+    public void render()
+    {
+        inputhandler();
+
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        //batch.draw(img, position.x, position.y);s
+        batch.end();
+
+        //camera.update();
+        //shapeRenderer.setProjectionMatrix(camera.combined);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 0, 0, 1);
+        shapeRenderer.line(position.x, position.y, mouse.x, mouse.y);
+        shapeRenderer.circle(position.x, position.y, 15);
+        shapeRenderer.end();
+
+    }
+
+    private void inputhandler()
+    {
+        mouse.set(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D))
+        {
+            position.x += 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A))
+        {
+            position.x -= 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W))
+        {
+            position.y += 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S))
+        {
+            position.y -= 1;
+        }
+
+    }
+
+    @Override
+    public void dispose()
+    {
+        batch.dispose();
+        img.dispose();
+        shapeRenderer.dispose();
+
+    }
 }
