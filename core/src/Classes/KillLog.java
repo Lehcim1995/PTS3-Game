8e25b89346881dc91fbfc71590f5231618ae9258
Sjobus <sibevanetten@hotmail.com>
2016-10-11 12:07:56 +0200

    KillLog
    KillLog op zet

---
+++ core/src/Classes/KillLog.java
@@ -0,0 +1,18 @@
+package Classes;
+
+/**
+ * Created by Sibe on 11-10-2016.
+ * In het klassendiagram heet deze klasse Killlog.
+ * Maar de drie kleine l'en achter elkaar is lastig lezen,
+ * dus heb ik hier de L van log een hoofd letter gemaakt.
+ */
+public class KillLog
+{
+   private Projectile projectile;
+   private Player player;
+
+    public void Killlog(Projectile projectile, Player player){
+        this.projectile = projectile;
+        this.player = player;
+    }
+}
