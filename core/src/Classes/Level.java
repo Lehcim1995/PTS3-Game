38fc3bfc7fe431b269b82e600b0f7c0bef8e95b4
Pigna <myron.antonissen@gmail.com>
2016-10-11 15:56:26 +0200

    Added Seed Generation, Added Method to create modules

--- core/src/Classes/Level.java
+++ core/src/Classes/Level.java
@@ -1,28 +1,72 @@
 package Classes;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Vector;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Shape;
+
 /**
  * Created by myron on 11-10-16.
  */
 public class Level
 {
-    private String seed;
+    private long seed;
+    private ArrayList<LevelBlock> levelBlocks;
+    private ArrayList<LevelBlock> blockModule;
+    private int levelSize = 4;
     //<editor-fold desc="Getters & Setters">
-    public String getSeed()
+
+    public ArrayList<LevelBlock> getLevelBlocks()
     {
-        return seed;
+        return levelBlocks;
     }
 
-    private void setSeed(String seed)
+    public void setLevelBlocks(ArrayList<LevelBlock> levelBlocks)
     {
-        this.seed = seed;
+        this.levelBlocks = levelBlocks;
+    }
+
+    public long getSeed()
+    {
+        return seed;
     }
     //</editor-fold>
     public Level()
     {
-        this.seed = "GenerateRandomString";
+        this.seed = GenerateSeed();
+        this.blockModule = CreateModules();
+        this.levelBlocks = GenerateLevelBlocks();
     }
-    public Level(String seed)
+    public Level(long seed)
     {
         this.seed = seed;
+        this.blockModule = CreateModules();
+        this.levelBlocks = GenerateLevelBlocks();
+    }
+    private long GenerateSeed()
+    {
+        Random random = new SecureRandom();
+        return random.nextLong();
+    }
+    private ArrayList<LevelBlock> GenerateLevelBlocks()
+    {
+        ArrayList<LevelBlock> newLevelBlockList = new ArrayList<LevelBlock>();
+        Random random = new Random(seed);
+        for(int i = 0; i < (levelSize * levelSize); i++)
+        {
+            newLevelBlockList.add(blockModule.get(random.nextInt(blockModule.size())));
+        }
+        return newLevelBlockList;
+    }
+    private ArrayList<LevelBlock> CreateModules()
+    {
+        ArrayList<LevelBlock> newLevelBlockList = new ArrayList<LevelBlock>();
+        //newLevelBlockList.add(new LevelBlock(null, new Vector2(),0,new Rectangle(0f,0f,50f,100f)));
+        return newLevelBlockList;
     }
 }
