98ea3b9dba0495984a34ad8a93f34983a0fe344e
Sjobus <sibevanetten@hotmail.com>
2016-11-29 10:19:18 +0100

    Login screen
    textfields voor de username en de password

--- core/src/Scenes/LoginScreen.java
+++ core/src/Scenes/LoginScreen.java
@@ -2,17 +2,17 @@ package Scenes;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
+
+import java.time.format.TextStyle;
 
 
 /**
@@ -23,6 +23,9 @@ public class LoginScreen extends AbstractScreen {
     private Texture txtrLogin;
     private TextureRegion myTextureRegion;
     private TextureRegionDrawable myTrd;
+    private TextField txtUserName;
+    private TextField txtPassword;
+    private Skin tfSkin;
 
     public LoginScreen()
     {
@@ -32,16 +35,29 @@ public class LoginScreen extends AbstractScreen {
 
     @Override
     public void buildStage() {
+        // todo fix die kk skin shit.
+        tfSkin = new Skin(Gdx.files.internal("uiskin.json"));
+        //TextField.TextFieldStyle tfs = new TextField.TextFieldStyle();
+        //tfs.font = tfSkin.getFont("tf");
+        txtUserName = new TextField("",tfSkin);
+        txtUserName.setMessageText("Username");
+        txtUserName.setPosition(260.f,300.f,Align.center);
+        txtPassword = new TextField("",tfSkin);
+        txtPassword.setMessageText("Password");
+        txtPassword.setPosition(260.f,200.f, Align.center);
         myTextureRegion = new TextureRegion(txtrLogin);
         myTrd = new TextureRegionDrawable(myTextureRegion);
         ImageButton btnLogin = new ImageButton(myTrd);
         btnLogin.setPosition(260.f, 40.f, Align.center);
+        addActor(txtUserName);
+        addActor(txtPassword);
         addActor(btnLogin);
 
         btnLogin.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event,
                                      float x, float y, int pointer, int button) {
+                System.out.println(txtUserName.getText());
                 ScreenManager.getInstance().showScreen(ScreenEnum.LOBBYLIST);
                 return false;
             }
