5937ec9287600f3715b93a7e2b0a15be8ad9c486
Lehcim1995 <micheljacobs@hotmail.nl>
2016-12-13 14:48:54 +0100

    More clean up

--- core/src/Classes/GameObject.java
+++ core/src/Classes/GameObject.java
@@ -3,15 +3,12 @@ package Classes;
 import Interfaces.IGameObject;
 import LibGDXSerialzableClasses.SerializablePolygon;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.Shape;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
