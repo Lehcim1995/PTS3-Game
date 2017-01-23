ed9793aacef76646d6c9648d21125b0e2691a7e1
Pigna <myron.antonissen@gmail.com>
2017-01-23 11:26:55 +0100

    test Gun init

---
+++ core/src/test/java/Classes/GunTest.groovy
@@ -0,0 +1,74 @@
+package Classes
+
+/**
+ * Created by myron on 23-01-17.
+ */
+class GunTest extends GroovyTestCase {
+    void testGetName() {
+
+    }
+
+    void testGetReloadTime() {
+
+    }
+
+    void testGetBulletsPerSecond() {
+
+    }
+
+    void testGetSpread() {
+
+    }
+
+    void testGetCurrentBullets() {
+
+    }
+
+    void testGetMaxBullets() {
+
+    }
+
+    void testGetShootType() {
+
+    }
+
+    void testIsHasInfinit() {
+
+    }
+
+    void testGetOwner() {
+
+    }
+
+    void testSetOwner() {
+
+    }
+
+    void testGetBulletSpeed() {
+
+    }
+
+    void testGetProjectileDamage() {
+
+    }
+
+    void testIsHasShot() {
+
+    }
+
+    void testSetHasShot() {
+
+    }
+
+    void testShoot() {
+
+    }
+
+    void testReload() {
+
+    }
+
+    void testToString() {
+
+    }
+}
