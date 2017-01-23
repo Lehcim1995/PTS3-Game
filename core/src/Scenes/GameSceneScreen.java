055a95c60cfdf927785fbd4ef8a578e361ecb91c
Nick Liebregts <n.liebregts@student.fontys.nl>
2017-01-23 11:43:05 +0100

    spectate no players

--- core/src/Scenes/GameSceneScreen.java
+++ core/src/Scenes/GameSceneScreen.java
@@ -148,7 +148,19 @@ public class GameSceneScreen extends AbstractScreen
 
         batch.begin();
 
-        GameManager.getInstance().getPlayer().DrawAmmo(batch);
+        if (GameManager.getInstance().getPlayer() != null)
+        {
+            GameManager.getInstance().getPlayer().DrawAmmo(batch);
+        }
+        else
+        {
+            try {
+                GameManager.getInstance().stop();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            ScreenManager.getInstance().showScreen(ScreenEnum.GAMELOBBY);
+        }
 
         font.setColor(Color.RED);
         layout.setText(font, GameManager.chat);
